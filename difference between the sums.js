3f728d5ebbde63bd05c38c88a8302e40180a6fbb
Eric de Anda <edeandae7@gmail.com>
2017-05-24 22:47:32 -0700

    project declaration

---
+++ difference between the sums.js
@@ -0,0 +1,14 @@
+/*
+ * the difference between the sum of the elements in the right subsequence and the sum of 
+ * the elements in the left subsequence is maximum difference possible
+ * from left and right sums get the right-left difference that would give the biggest difference
+ * 
+ */
+//    0   1  2  3      4   5   6   7  8
+// v: 3, -5, 1, -2,     8, -2, 3, -2, 1
+//     L: -5, 1, -2   R: 8, -2, 3.
+
+//[4,5,6,49,1,45,34,56] - [110]
+
+var integerArray=[4,5,6,49,1,45,34,56,110]
+
