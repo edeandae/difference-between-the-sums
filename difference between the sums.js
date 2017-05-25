1f1456a2d2c294b976afebc1361c73121ebb96ab
Eric de Anda <edeandae7@gmail.com>
2017-05-25 12:50:10 -0700

    the arraySum correctly adds all the values inside the given range of the globla array maxDifferenceCheck correctly checks if the two given ranges yield the maximum difference so far and if so modify the max variables to be those ranges and the resulting maxDifference

--- difference between the sums.js
+++ difference between the sums.js
@@ -12,3 +12,39 @@
 
 var integerArray=[4,5,6,49,1,45,34,56,110]
 
+var maxDifference=0
+var maxDifferenceFirstStart=0
+var maxDifferenceFirstFinish=0
+var maxDifferenceSecondStart=0
+var maxDifferenceSecondFinish=0
+
+
+function arraySum(start, finish){
+	var sum=0;
+	for(i=start;i<=finish;i++){
+		sum=sum+integerArray[i]
+	}
+	return sum
+}
+
+function maxDifferenceCheck(firstStart, firstFinish, secondStart, secondFinish){
+	if(Math.abs(arraySum(firstStart,firstFinish)-arraySum(secondStart,secondFinish))>maxDifference){
+		maxDifference=Math.abs(arraySum(firstStart,firstFinish)-arraySum(secondStart,secondFinish))
+		maxDifferenceFirstStart=firstStart
+		maxDifferenceFirstFinish=firstFinish
+		maxDifferenceSecondStart=secondStart
+		maxDifferenceSecondFinish=secondFinish
+	}
+
+}
+
+console.log(arraySum(0,2,integerArray))
+
+maxDifferenceCheck(0,0,2,3)
+console.log(maxDifference)
+console.log(maxDifferenceFirstStart)
+console.log(maxDifferenceFirstFinish)
+console.log(maxDifferenceSecondStart)
+console.log(maxDifferenceSecondFinish)
+
+//console.log(Math.abs(-3))
